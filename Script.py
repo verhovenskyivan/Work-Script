daedd0ddbd9eae0ebbd2124031dd359861eedb7d
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-04-26 19:16:40 +0300

    Delete Script.py

--- Script.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
-options = Options()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager.install()),
-                          options=Options)
