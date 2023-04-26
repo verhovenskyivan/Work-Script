8ecd560672105a03c843944cd77ddb1bd5b7d925
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-04-26 19:18:37 +0300

    Add files via upload

--- Secret.py
+++ Secret.py
@@ -1,5 +1,4 @@
-email = ""
-password = ""
-val = input("Enter pack: ")
-Pack = val
-link = ""
+email = input("Enter email: ")
+password = input("Enter password: ")
+Pack = input("Enter pack: ")
+link = input("Enter link: ")
