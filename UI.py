384293172428f049415cc2b56159fdd259003ca1
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-05-01 18:00:15 +0300

    Add files via upload

--- UI.py
+++ UI.py
@@ -1,36 +1,35 @@
 import tkinter, customtkinter
+from PIL import Image, ImageTk
+
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
 
-root.geometry("500x350")
+root.geometry("300x300")
+image = Image.open("interface/latech.png")
+resize_image = image.resize((300,300),resample=3)
 
 def login():
-    print("Enter login: ")
-
+    input("Enter login: ")
 
 frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=20, fill="both", expand=True)
-
-label= customtkinter.CTkLabel(master=frame, text="Login system", text_font=("Robot,24"))
-label.pack(pady=12,padx=10)
-
+frame.pack(pady=0, padx=0, fill="both", expand=True)
 
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Login")
-entry1.pack(pady=12, padx=10)
+entry1.pack(pady=10, padx=10)
 
 entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
-entry2.pack(pady=12,padx=10)
+entry2.pack(pady=15,padx=10)
 
 entry3 = customtkinter.CTkEntry(master=frame, placeholder_text="Pups link")
-entry3.pack(pady=12,padx=15)
+entry3.pack(pady=10,padx=10)
 
 button = customtkinter.CTkButton(master=frame, text="Start", command=login)
-button.pack(pady=12,padx=10)
+button.pack(pady=10,padx=10)
 
 checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember me")
-checkbox.pack(pady=12,padx=10)
+checkbox.pack(pady=10,padx=10)
 
 root.mainloop()
