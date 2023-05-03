8511a4dda4f1056fa6d455d991b76d71ee4feaa5
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-05-03 23:37:09 +0300

    Add files via upload

--- WorkScript.py
+++ WorkScript.py
@@ -1,14 +1,11 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from getpass import getpass
-from multiprocessing import Process
-from win10toast import ToastNotifier
 import time
-
 
 s = Service('./WorkScript/chromedriver.exe')                                                              
 
@@ -18,9 +15,6 @@ options.add_argument("--headless=new")
 
 driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
-toast = ToastNotifier()
-toast.show_toast("Process started")
-
 with open('Config.txt','r') as file:
    for details in file:
       email, password  = details.split(',')
@@ -28,42 +22,25 @@ with open('Config.txt','r') as file:
 with open('Link.txt','r') as f:
       link  = f.readline()
 
-Pack = input("Enter pack: ")
+Pack = input("Enter pack: ").split(",")
 
-def Get_Link():
-   driver.get(link)
-
-def User():
-   driver.find_element(By.ID, 'identity').send_keys(email)
-   driver.find_element(By.ID, 'credential').send_keys(password)
+driver.get(link)
 
 def Pack_Find():
+   driver.find_element(By.ID, 'identity').send_keys(email)
+   driver.find_element(By.ID, 'credential').send_keys(password)
    driver.find_element(By.NAME, "submit").send_keys(Keys.ENTER)
-   driver.find_element(By.CLASS_NAME, "form-control").send_keys(Pack)
  
 def Pack_Delete():
+   driver.find_element(By.CLASS_NAME, "form-control").send_keys(Pack)
    driver.find_element(By.NAME,"submit0").send_keys(Keys.ENTER)
    driver.find_element(By.CLASS_NAME, "jq-remove-pack-button").send_keys(Keys.ENTER)
    driver.switch_to.alert.accept()
    
-def Script_First_Part():
-   Get_Link()
-   User()
-
-def Script_Second_Part():
-   Pack_Find()
-   Pack_Delete()
-   time.sleep(1)
-   driver.refresh()
-   
-if driver.refresh:
-   toast.show_toast(Pack,"Process finished")
-   Pack
-   User()
-   Script_Second_Part()
-   
-Pack
-Script_First_Part()
-Script_Second_Part()
+Pack_Find()
+Pack_Delete()
+time.sleep(1)
 
 
+
+     
