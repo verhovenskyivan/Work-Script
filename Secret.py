52fcf5b081fdcb13473b246246e6a35e7501204e
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-05-11 00:51:49 +0300

    Update Secret.py

--- Secret.py
+++ Secret.py
@@ -1,9 +1,10 @@
 import WorkScript
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from WorkScript import *
 
-with open('Config.txt','r') as file:
-    for details in file:
-        email, password = details.split(',')
-
-with open('Link.txt','r') as file:
-    for details in file:
-        link = details
