e737aa31c2edc9253551af0fac8d8e2e4f677dfa
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-05-14 19:21:17 +0300

    Add files via upload

--- WorkScript.py
+++ WorkScript.py
@@ -6,9 +6,10 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time, sys, os, subprocess, logging
-import re 
+import re
 from tkinter import Tk
 import io
+import keyboard
 
 s = Service('./WorkScript/chromedriver.exe')                                                              
 
@@ -21,53 +22,58 @@ driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager()
 with io.open('Config.txt','r') as file:
    for details in file:
       email, password  = details.split(',')
-        
-with io.open('Link.txt','r') as f:
-      link  = f.readline()
-
-Packlist = input ("Enter pack: ")
 
 win = Tk()
 
-cliptext = win.clipboard_get().split('/')
-Link_Start = "https://"
-Pack_Delition_sublink = "/tools/unknownpackremoval"
-Pups_Link = cliptext[2]
-Full_link = Link_Start + Pups_Link + Pack_Delition_sublink
-for link in cliptext.split('/'):
-   print(Full_link)
-
-driver.get(Full_link)
-
+def Pups():
+  cliptext = win.clipboard_get().split('/')
+  Link_Start = "https://"
+  Pack_Delition_sublink = "/tools/unknown_pack_removal"
+  Pups_Link = cliptext[2]
+  Full_link = Link_Start + Pups_Link + Pack_Delition_sublink
+  print(Full_link)
+ 
+  driver.get(Full_link)
+ 
 def Pack_Find():
-   driver.find_element(By.ID, 'identity').send_keys(email)#Ввод логина
-   driver.find_element(By.ID, 'credential').send_keys(password)#Ввод пароля
-   driver.find_element(By.NAME, "submit").send_keys(Keys.ENTER)#Авторизация
+    driver.find_element(By.ID, 'identity').send_keys(email)#Ввод логина
+    driver.find_element(By.ID, 'credential').send_keys(password)#Ввод пароля
+    driver.find_element(By.NAME, "submit").send_keys(Keys.ENTER)#Авторизация
 
 def Pack_Delete():
      for Pack in re.split('[";|,|:|\n|\\| "]',Packlist): 
-       if Pack != '': 
+        if Pack != '': 
          print(Pack)
          driver.find_element(By.CLASS_NAME, "form-control").send_keys(Pack)#Ввод в графу поиска
          driver.find_element(By.NAME,"submit0").send_keys(Keys.ENTER)#Нажатие кнопки поиск
          try:
             driver.find_element(By.CLASS_NAME, "jq-remove-pack-button").send_keys(Keys.ENTER)#Нажатие кнопки удаление
             driver.switch_to.alert.accept()#Свич на алерт и его принятие 
-            print(Pack + "Пак удален")
+            print(Pack + " Пак удален")
             driver.find_element(By.CLASS_NAME, "form-control").clear()
          except NoSuchElementException:#Обработка ошибки
-            print(Pack + "Пак не удален")
+            print(Pack + " Пак не удален")
             driver.find_element(By.CLASS_NAME, "form-control").send_keys(Pack)
             driver.find_element(By.NAME,"submit0").send_keys(Keys.ENTER)
             driver.find_element(By.CLASS_NAME, "form-control").clear()
+            
+while True:
+   
+ print("Скопируй ссылку и нажми Alt + D")  
+ keyboard.wait("Alt + D")        
+ Pups()
+ print("Скопируй паки и нажми Alt + D")
+ keyboard.wait("Alt + D")
+ 
+ Packlist = win.clipboard_get()
+ Pack_Find()
+ Pack_Delete()
+ time.sleep(1)
+ 
+ subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
 
-Pack_Find()
-Pack_Delete()
-time.sleep(1)
-subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
-
-logging.info("INFO")
-logging.warning("WARNING")
-logging.basicConfig(level=logging.WARNING, filename="Logger.log",filemode="a",
+ logging.info("INFO")
+ logging.warning("WARNING")
+ logging.basicConfig(level=logging.WARNING, filename="Logger.log",filemode="a",
                    format="%(asctime)s %(levelname)s %(message)s")
-logging.error("Error", exc_info=True)
+ logging.error("Error", exc_info=True)
