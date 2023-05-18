f93aa10d7d42b415f1ce299b720c8882038e7eb8
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-05-18 13:48:10 +0300

    changed buttons

--- Main.py
+++ Main.py
@@ -18,12 +18,21 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="Вводи паки сюда:")
-        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="Вводи паки сюда:", width=100, height=300)
+        self.entry.grid(row=1, column=1, columnspan=1, padx=(0, 500), pady=(200, 0), sticky="nsew")
+        
+        self.button = customtkinter.CTkButton(self, text="Удалить паки")
+        self.button.grid(row=3, column=1, columnspan=1, padx=(500,0), pady=(20, 20), sticky="nsew")
+         
+        self.button = customtkinter.CTkButton(self, text="Пометить паки недопоставкой")
+        self.button.grid(row=4, column=1, columnspan=1, padx=(500,0), pady=(20, 20), sticky="nsew")
+        
+        self.button = customtkinter.CTkButton(self, text="Перенести паки в зону ДВ")
+        self.button.grid(row=5, column=1, columnspan=1, padx=(500,0), pady=(20, 20), sticky="nsew")
         
 
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid(row=0, column=0, rowspan=6, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Pups Helper", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
@@ -38,13 +47,9 @@ class App(customtkinter.CTk):
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "100%"])
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
         
+       
         
-        self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="Основное окно")
-        self.scrollable_frame.grid(row=0, column=1, padx=(10, 0), pady=(20, 0), sticky="nsew")
-        self.scrollable_frame.grid_columnconfigure(0, weight=1)
-        self.scrollable_frame_switches = []
         
-
 if __name__ == "__main__":
     app = App()
     app.mainloop()
